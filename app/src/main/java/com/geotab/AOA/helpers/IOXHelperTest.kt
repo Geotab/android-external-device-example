4898bb092ccc098a2a61740e938fbf1f6713554a
H-Jamshidi <hosseinjamshidi@geotab.com>
2023-07-26 19:53:41 -0400

    update IOXHelperTest

--- app/src/main/java/com/geotab/AOA/helpers/IOXHelperTest.kt
+++ app/src/main/java/com/geotab/AOA/helpers/IOXHelperTest.kt
@@ -1,13 +1,21 @@
 package com.geotab.AOA.helpers
 
 import com.geotab.AOA.helpers.IOXHelper.Companion.getIOXTopicListMessage
-import com.geotab.AOA.helpers.IOXHelper.Companion.toHexString
+import com.geotab.ioxproto.IoxMessaging.IoxToGo
+import org.testng.AssertJUnit.assertEquals
+import org.testng.AssertJUnit.assertTrue
 import org.testng.annotations.Test
-
 
 internal class IOXHelperTest{
     @Test
     fun testAll(){
-        println("getIOXTopicListMessage: ${getIOXTopicListMessage().toHexString()}")
+        println("getIOXTopicListMessage: ${getIOXTopicListMessage()}")
+    }
+
+    @Test
+    fun testGetIOXTopicListMessage(){
+        val actual = getIOXTopicListMessage()
+        assertEquals("testGetIOXTopicListMessage", actual.msgCase, IoxToGo.MsgCase.PUB_SUB)
+        assertTrue("testGetIOXTopicListMessage", actual.pubSub.hasListAvailTopics())
     }
 }
