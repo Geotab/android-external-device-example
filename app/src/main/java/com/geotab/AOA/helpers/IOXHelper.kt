369f9cabc79eb57fbce097893e9a8ab22b78bc8a
H-Jamshidi <hosseinjamshidi@geotab.com>
2023-08-01 08:19:46 -0400

    update IOXHelper

--- app/src/main/java/com/geotab/AOA/helpers/IOXHelper.kt
+++ app/src/main/java/com/geotab/AOA/helpers/IOXHelper.kt
@@ -1,15 +1,24 @@
 package com.geotab.AOA.helpers
 
+import com.geotab.ioxproto.IoxMessaging
 import com.geotab.ioxproto.IoxMessaging.IoxToGo
 import com.geotab.ioxproto.IoxMessaging.PubSubToGo
+import com.geotab.ioxproto.IoxMessaging.Subscribe
 import com.google.protobuf.Empty
 
 class IOXHelper {
+
     companion object {
-        fun getIOXTopicListMessage(): IoxToGo {
-            return IoxToGo
-                .newBuilder()
+        public fun getIOXTopicListMessage(): IoxToGo {
+            return IoxToGo.newBuilder()
                 .setPubSub(PubSubToGo.newBuilder().setListAvailTopics(Empty.getDefaultInstance()))
+                .build()
+        }
+
+        public fun getIOXSubscribeToGPSMessage(): IoxToGo {
+            return IoxToGo.newBuilder()
+                .setPubSub(PubSubToGo.newBuilder().setSub(Subscribe.newBuilder().setTopicValue(
+                    IoxMessaging.Topic.TOPIC_GEAR_VALUE)))
                 .build()
         }
 
