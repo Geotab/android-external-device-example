04a56ad6b4ba993573e89ac67dae90189d8a8e45
H-Jamshidi <hosseinjamshidi@geotab.com>
2023-08-01 10:59:19 -0400

    add onPassthroughReceived to IOXListener

--- app/src/main/java/com/geotab/AOA/IOXListener.kt
+++ app/src/main/java/com/geotab/AOA/IOXListener.kt
@@ -4,4 +4,7 @@ import com.geotab.ioxproto.IoxMessaging
 
 interface IOXListener {
     fun onIOXReceived(message : IoxMessaging.IoxFromGo)
+    fun onStatusUpdate(message : String)
+    fun onUpdateHOSText(dataHOS : HOSData)
+    fun onPassthroughReceived(message : String)
 }
