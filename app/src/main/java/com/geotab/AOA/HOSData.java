1c3198ab88cea98d4c93c08d8ab8b2bad1d37966
H-Jamshidi <hosseinjamshidi@geotab.com>
2023-08-01 10:58:44 -0400

    extract HOSData

---
+++ app/src/main/java/com/geotab/AOA/HOSData.java
@@ -0,0 +1,16 @@
+package com.geotab.AOA;
+
+public class HOSData {
+    public String sDateTime;
+    public float Latitude;
+    public float Logitude;
+    public int iRoadSpeed;
+    public int iRPM;
+    public int iOdometer;
+    public String sStatus;
+    public int iTripOdometer;
+    public int iEngineHours;
+    public int iTripDuration;
+    public int iVehicleId;
+    public int iDriverId;
+}
