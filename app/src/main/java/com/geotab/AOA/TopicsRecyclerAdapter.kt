372d6807a86ce65d9188c840f72444a3d27c7fa8
H-Jamshidi <hosseinjamshidi@geotab.com>
2023-08-02 13:49:29 -0400

    apply code style

--- app/src/main/java/com/geotab/AOA/TopicsRecyclerAdapter.kt
+++ app/src/main/java/com/geotab/AOA/TopicsRecyclerAdapter.kt
@@ -9,8 +9,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class TopicsRecyclerAdapter(private val dataSet: List<TopicsDataModel>, private val metrics: DisplayMetrics,
-                            private val listener: (TopicsDataModel, Int) -> Unit) :
+class TopicsRecyclerAdapter(
+    private val dataSet: List<TopicsDataModel>, private val metrics: DisplayMetrics,
+    private val listener: (TopicsDataModel, Int) -> Unit
+) :
     RecyclerView.Adapter<TopicsRecyclerAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val lblSub: TextView
@@ -18,13 +20,14 @@ class TopicsRecyclerAdapter(private val dataSet: List<TopicsDataModel>, private 
         val counterText: TextView
         val statusText: TextView
         val topicLayout: LinearLayout
+
         init {
             // Define click listener for the ViewHolder's View
             lblSub = view.findViewById(R.id.lbl_sub)
             valueText = view.findViewById(R.id.topic_data)
-            statusText  = view.findViewById(R.id.topic_sub_status)
-            counterText  = view.findViewById(R.id.topic_counter)
-            topicLayout  = view.findViewById(R.id.topic_sub_layout)
+            statusText = view.findViewById(R.id.topic_sub_status)
+            counterText = view.findViewById(R.id.topic_counter)
+            topicLayout = view.findViewById(R.id.topic_sub_layout)
         }
     }
 
@@ -38,19 +41,21 @@ class TopicsRecyclerAdapter(private val dataSet: List<TopicsDataModel>, private 
         viewHolder.lblSub.text = dataSet[position].name
         viewHolder.lblSub.setTypeface(null, dataSet[position].subscribed.typeface);
         viewHolder.itemView.setOnClickListener { listener(dataSet[position], position) }
-        val valueTextStr ="Value: ${dataSet[position].dataText}"
-        viewHolder.valueText.text =valueTextStr
-        val counterTextStr ="Counter: ${dataSet[position].counter}"
-        viewHolder.counterText.text =counterTextStr
-        viewHolder.statusText.text= dataSet[position].subscribed.statusText
+        val valueTextStr = "Value: ${dataSet[position].dataText}"
+        viewHolder.valueText.text = valueTextStr
+        val counterTextStr = "Counter: ${dataSet[position].counter}"
+        viewHolder.counterText.text = counterTextStr
+        viewHolder.statusText.text = dataSet[position].subscribed.statusText
         viewHolder.statusText.setTextColor(dataSet[position].subscribed.color)
-        viewHolder.statusText.setTypeface(null, dataSet[position].subscribed.typeface);
-        viewHolder.topicLayout.elevation = convertDipsToPixels(dataSet[position].subscribed.layoutElv)
+        viewHolder.statusText.setTypeface(null, dataSet[position].subscribed.typeface)
+        viewHolder.topicLayout.elevation =
+            convertDipsToPixels(dataSet[position].subscribed.layoutElv)
     }
 
     private fun convertDipsToPixels(dp: Float): Float {
         return dp * metrics.density
     }
+
     override fun getItemCount() = dataSet.size
 
     override fun getItemId(position: Int): Long {
